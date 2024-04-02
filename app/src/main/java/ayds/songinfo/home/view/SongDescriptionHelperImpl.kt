92388de41572f866731034a6cc808a6e7ecd17e8
Katherina <katherinavilca@gmail.com>
2024-04-02 17:28:43 -0300

    cambio de year por release date en la caja de descripcion de la cancion + funcion createDate

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -8,7 +8,7 @@ interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
 }
 
-internal class SongDescriptionHelperImpl : SongDescriptionHelper {
+internal class SongDescriptionHelperImpl(val dateFormatFactory: DateFormatFactory) : SongDescriptionHelper {
     override fun getSongDescriptionText(song: Song): String {
         return when (song) {
             is SpotifySong ->
@@ -18,8 +18,13 @@ internal class SongDescriptionHelperImpl : SongDescriptionHelper {
                 }\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Release Date: ${song.createDate()}"
             else -> "Song not found"
         }
+    }
+
+    private fun SpotifySong.createDate(): String{
+        val dateFactory = dateFormatFactory.getDate(this.releaseDatePrecision, this.releaseDate)
+        return dateFactory.createDate()
     }
 }
