838d446b38cca147addb3b36efeb8d7a0abe445f
Katherina <katherinavilca@gmail.com>
2024-04-22 09:30:57 -0300

    Rename de funciones, clases format y creacion de constantes para distinguir indices (day, month, year)

--- app/src/main/java/ayds/songinfo/home/view/DateFormatFactory.kt
+++ app/src/main/java/ayds/songinfo/home/view/DateFormatFactory.kt
@@ -2,62 +2,64 @@ package ayds.songinfo.home.view
 
 import java.time.Month
 
+private const val DAY_INDEX= 2
+private const val MONTH_INDEX = 1
+private const val YEAR_INDEX = 0
 interface DateFormatFactory {
-    fun getDate(precision:String,date:String ): DateSong
+    fun getDateFormat(precision:String,date:String ): DateFormat
 }
 
 object DateFormatFactoryImpl: DateFormatFactory{
     private var dateArray =  emptyArray<String>()
 
-   override fun getDate(precision:String, date:String):DateSong{
+   override fun getDateFormat(precision:String, date:String):DateFormat{
 
         dateArray = date.split("-").toTypedArray()
         return when (precision){
-            "day"-> DayFactory(dateArray)
-            "month" -> MonthFactory(dateArray)
-            "year" -> YearFactory(dateArray)
-            else -> DefautFactory(dateArray)
+            "day"-> DayFormat(dateArray)
+            "month" -> MonthFormat(dateArray)
+            "year" -> YearFormat(dateArray)
+            else -> DefaultFormat(dateArray)
         }
     }
 }
 
-sealed class DateSong{
+sealed class DateFormat{
     abstract fun createDate():String
 
 }
 
-private class DayFactory(val dateArray: Array<String>): DateSong() {
+private class DayFormat(val dateArray: Array<String>): DateFormat() {
     override fun createDate(): String {
-        val day = dateArray[2]
-        val month = dateArray[1]
-        val year = dateArray[0]
+        val day = dateArray[DAY_INDEX]
+        val month = dateArray[MONTH_INDEX]
+        val year = dateArray[YEAR_INDEX]
         return "$day/$month/$year"
     }
 }
 
-private class MonthFactory(val dateArray: Array<String>): DateSong(){
+private class MonthFormat(val dateArray: Array<String>): DateFormat(){
     override fun createDate(): String {
-        val month = dateArray[1].toInt()
-        val year = dateArray[0]
+        val month = dateArray[MONTH_INDEX].toInt()
+        val year = dateArray[YEAR_INDEX]
         val nameMonth = Month.of(month).toString()
         return "$nameMonth, $year"
     }
 }
 
-private class YearFactory(val dateArray: Array<String>): DateSong(){
+private class YearFormat(val dateArray: Array<String>): DateFormat(){
     override fun createDate(): String {
-        val year = dateArray[0].toInt()
-        val isLeapYear = isLeapYear(year)
-        return "$year $isLeapYear"
+        val year = dateArray[YEAR_INDEX].toInt()
+        return "$year ${ if (isLeapYear(year)) "leap year" else "not leap year"}"
     }
 
-    fun isLeapYear(year: Int): String{
-        val result = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-        return if (result) "leap year" else "not leap year"
+    fun isLeapYear(year: Int): Boolean{
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     }
+
 }
 
-private class DefautFactory(val dateArray: Array<String>): DateSong(){
+private class DefaultFormat(val dateArray: Array<String>): DateFormat(){
     override fun createDate(): String {
         return dateArray.joinToString("-")
     }
