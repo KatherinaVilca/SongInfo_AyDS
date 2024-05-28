75159b5abda6589e18483b655f1d9826579aa17b
Katherina <katherinavilca@gmail.com>
2024-05-28 01:43:30 -0300

    update del injector de moredetails. Ya no inicializa a lastfm, directamente invoca a su inyector y luego pide la variable para pasarle al repositorio

--- app/src/main/java/ayds/songinfo/moredetails/fulllogic/injector/OtherInfoInjector.kt
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/injector/OtherInfoInjector.kt
@@ -2,37 +2,30 @@ package ayds.songinfo.moredetails.fulllogic.injector
 
 import android.content.Context
 import androidx.room.Room
+import ayds.artist.external.lastFM.injector.LastFMInjector
 import ayds.songinfo.moredetails.fulllogic.data.OtherInfoRepositoryImpl
-import ayds.songinfo.moredetails.fulllogic.data.external.LastFMAPI
-import ayds.songinfo.moredetails.fulllogic.data.external.LastFMToArtistBiographyResolverImpl
-import ayds.songinfo.moredetails.fulllogic.data.external.OtherInfoServiceImpl
+
 import ayds.songinfo.moredetails.fulllogic.data.localdb.ArticleDatabase
 import ayds.songinfo.moredetails.fulllogic.data.localdb.OtherInfoLocalStorageImpl
 import ayds.songinfo.moredetails.fulllogic.presentation.ArtistBiographyDescriptionHelperImpl
 import ayds.songinfo.moredetails.fulllogic.presentation.OtherInfoPresenter
 import ayds.songinfo.moredetails.fulllogic.presentation.OtherInfoPresenterImpl
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
-private const val LASTFM_URL = "https://ws.audioscrobbler.com/2.0/"
+
+
 private const val ARTICLE_DATABASE_NAME = "database-name-thename"
 object OtherInfoInjector {
 
    lateinit var presenter : OtherInfoPresenter
    fun init(context : Context){
 
-       val retrofit = Retrofit.Builder()
-            .baseUrl(LASTFM_URL)
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-       val lastFMAPI = retrofit.create(LastFMAPI::class.java)
+       LastFMInjector.init()
+
        val articleDataBase = Room.databaseBuilder(context, ArticleDatabase::class.java, ARTICLE_DATABASE_NAME).build()
-       val lastFMToArtistBiographyResolver = LastFMToArtistBiographyResolverImpl()
 
         // data
-       val service = OtherInfoServiceImpl(lastFMToArtistBiographyResolver, lastFMAPI)
        val localStorage = OtherInfoLocalStorageImpl(articleDataBase)
-       val repository = OtherInfoRepositoryImpl(localStorage, service)
+       val repository = OtherInfoRepositoryImpl(localStorage, LastFMInjector.lastFMService)
 
        //presentation
        val artistBiographyDescriptionHelper = ArtistBiographyDescriptionHelperImpl()
