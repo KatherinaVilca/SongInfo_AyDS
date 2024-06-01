0c6d6929749a0ccdb589764b8af801303daf32fb
Katherina <katherinavilca@gmail.com>
2024-06-01 02:02:58 -0300

    modifique funciones para que pidan/inserten mas de una card a la vez

--- app/src/main/java/ayds/songinfo/moredetails/fulllogic/data/localdb/OtherInfoLocalStorage.kt
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/data/localdb/OtherInfoLocalStorage.kt
@@ -4,32 +4,42 @@ package ayds.songinfo.moredetails.fulllogic.data.localdb
 import ayds.songinfo.moredetails.fulllogic.domain.entity.Card
 
 interface OtherInfoLocalStorage {
-    fun getCard(artistName: String): Card?
-    fun insertCard(artistBiography: Card, artistName: String)
+    fun getCard(artistName: String): ArrayList<Card>?
+    fun insertCard(listCard: ArrayList<Card>, artistName: String)
 }
 
 internal class OtherInfoLocalStorageImpl (
     private var cardDataBase: CardDatabase
 ): OtherInfoLocalStorage {
 
-    override fun getCard(artistName: String): Card? {
-        val cardEntity = cardDataBase.CardDao().getArticleByArtistName(artistName)
-        return cardEntity?.let{
-            Card(
-                cardEntity.artistName,
-                cardEntity.text,
-                cardEntity.url,
-                cardEntity.source,
-                "")}
+    override fun getCard(artistName: String): ArrayList<Card>? {
+        val cardsEntities = cardDataBase.CardDao().getCardByArtistName(artistName)
+        val cards = ArrayList<Card> ()
+
+        if (cardsEntities != null) {
+            for (cardEntity in cardsEntities) {
+                cards.add(
+                    Card(
+                        cardEntity.artistName,
+                        cardEntity.text,
+                        cardEntity.url,
+                        cardEntity.source,
+                        "")
+                )}
+        }
+        return cards
     }
 
-    override fun insertCard(card: Card, artistName: String) {
-       cardDataBase.CardDao().insertArticle(
-            CardEntity(
-                card.artistName,
-                card.text,
-                card.infoUrl,
-                card.source
-            ))
+    override fun insertCard(listCard: ArrayList<Card>, artistName: String) {
+        for (card in listCard) {
+            cardDataBase.CardDao().insertCard(
+                CardEntity(
+                    card.artistName,
+                    card.text,
+                    card.infoUrl,
+                    card.source
+                )
+            )
+        }
     }
 }
