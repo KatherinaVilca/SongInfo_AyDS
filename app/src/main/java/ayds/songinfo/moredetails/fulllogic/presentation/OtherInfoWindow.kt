5b7ccdf8e4f758d655f9fa48b67e153c1f516d46
Katherina <katherinavilca@gmail.com>
2024-06-06 20:49:09 -0300

    agregue componentes para que ahora se puedan visibilizar los datos de los tres servicios

--- app/src/main/java/ayds/songinfo/moredetails/fulllogic/presentation/OtherInfoWindow.kt
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/presentation/OtherInfoWindow.kt
@@ -41,6 +41,14 @@ class OtherInfoWindow : Activity() {
         card1TextView = findViewById(R.id.card1TextView)
         card1buttonOpenArticle = findViewById(R.id.card1buttonOpenArticle)
         card1Image = findViewById(R.id.card1Image)
+
+        card2TextView = findViewById(R.id.card2TextView)
+        card2buttonOpenArticle = findViewById(R.id.card2buttonOpenArticle)
+        card2Image = findViewById(R.id.card2Image)
+
+        card3TextView = findViewById(R.id.card3TextView)
+        card3buttonOpenArticle = findViewById(R.id.card3buttonOpenArticle)
+        card3Image = findViewById(R.id.card3Image)
     }
 
     private fun initPresenter(){
@@ -67,15 +75,35 @@ class OtherInfoWindow : Activity() {
         otherInfoPresenter.getCardList(artistName!!)
     }
 
-    private fun updateUi (listcardUiState: ArrayList<CardUiState>){
+    private fun updateUi(listcardUiState: ArrayList<CardUiState>) {
         runOnUiThread {
-            updateButtonArticle(listcardUiState.first().infoUrl)
-            updateLogoLastFM(listcardUiState.first().imageUrl)
-            updateArticleText(listcardUiState.first().contentHtml)
+            for (i in 0 until listcardUiState.size) {
+               when(i){
+                   0 -> updateCar1(listcardUiState[0])
+                   1 -> updateCar2(listcardUiState[1])
+                   2 -> updateCar3(listcardUiState[2])
+               }
+            }
         }
     }
 
-    private fun updateButtonArticle(articleUrl: String){
+    private fun updateCar1(cardUiState: CardUiState){
+        updateButtonArticleCard1(cardUiState.infoUrl)
+        updateLogoCard1(cardUiState.imageUrl)
+        updateArticleTextCard1(cardUiState.contentHtml)
+    }
+    private fun updateCar2(cardUiState: CardUiState){
+        updateButtonArticleCard2(cardUiState.infoUrl)
+        updateLogoCard2(cardUiState.imageUrl)
+        updateArticleTextCard2(cardUiState.contentHtml)
+    }
+    private fun updateCar3(cardUiState: CardUiState){
+        updateButtonArticleCard3(cardUiState.infoUrl)
+        updateLogoCard3(cardUiState.imageUrl)
+        updateArticleTextCard3(cardUiState.contentHtml)
+    }
+
+    private fun updateButtonArticleCard1(articleUrl: String){
         card1buttonOpenArticle.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.setData(Uri.parse(articleUrl))
@@ -83,14 +111,45 @@ class OtherInfoWindow : Activity() {
         }
     }
 
-    private fun updateLogoLastFM(imageUrl: String){
+    private fun updateLogoCard1(imageUrl: String){
         Picasso.get().load(imageUrl).into(   card1Image  )
     }
 
-    private fun updateArticleText(articleHtml: String){
+    private fun updateArticleTextCard1(articleHtml: String){
         card1TextView.text = Html.fromHtml(articleHtml)
     }
 
+    private fun updateButtonArticleCard2(articleUrl: String){
+        card2buttonOpenArticle.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(articleUrl))
+            startActivity(intent)
+        }
+    }
+
+    private fun updateLogoCard2(imageUrl: String){
+        Picasso.get().load(imageUrl).into(   card2Image  )
+    }
+
+    private fun updateArticleTextCard2(articleHtml: String){
+        card2TextView.text = Html.fromHtml(articleHtml)
+    }
+
+    private fun updateButtonArticleCard3(articleUrl: String){
+        card3buttonOpenArticle.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(articleUrl))
+            startActivity(intent)
+        }
+    }
+
+    private fun updateLogoCard3(imageUrl: String){
+        Picasso.get().load(imageUrl).into(   card3Image  )
+    }
+
+    private fun updateArticleTextCard3(articleHtml: String){
+        card3TextView.text = Html.fromHtml(articleHtml)
+    }
     companion object {
         const val ARTIST_NAME_EXTRA = "artistName"
     }
