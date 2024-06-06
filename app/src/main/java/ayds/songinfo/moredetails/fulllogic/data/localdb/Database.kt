64b775b722b48c9babcd1a57a49a976106abdb37
Katherina <katherinavilca@gmail.com>
2024-06-06 18:53:36 -0300

    cambio en la base de datos para guardar varias entradas para un artista y poder recuperarlas

--- app/src/main/java/ayds/songinfo/moredetails/fulllogic/data/localdb/Database.kt
+++ app/src/main/java/ayds/songinfo/moredetails/fulllogic/data/localdb/Database.kt
@@ -5,22 +5,22 @@ import androidx.room.Database
 import androidx.room.Entity
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
-import androidx.room.PrimaryKey
 import androidx.room.Query
 import androidx.room.RoomDatabase
 
-    @Database(entities = [CardEntity::class], version = 1)
+@Database(entities = [CardEntity::class], version = 1)
 abstract class CardDatabase : RoomDatabase() {
     abstract fun CardDao(): CardDao
 }
 
-@Entity
+@Entity(primaryKeys = ["artistName", "source"])
 data class CardEntity(
-    @PrimaryKey
+
     val artistName: String,
     val text: String,
     val url: String,
-    val source: String
+    val source: String,
+    val sourceLogoUrl: String
 )
 
 @Dao
